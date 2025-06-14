7f2bbc6ed2858bbbc4ba879cd8d54327a500d329
genus555 <yunxiaoche@gmail.com>
2025-06-13 21:45:08 -0700

    can count characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -9,5 +9,6 @@ def main():
     content = get_book_text("/mnt/d/code/github/bookbot/books/frankenstein.txt")
     word_count = count_words(content)
     print(f"{word_count} words found in the document")
+    print(count_characters(content))
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,13 @@ def count_words(content):
     for word in words:
         word_count += 1
     return word_count
+
+def count_characters(content):
+    content = content.lower()
+    chars = {}
+    for character in content:
+        if character != " " and character not in chars:
+            chars[character]  = 1
+        elif character != " " and character in chars:
+            chars[character] += 1
+    return chars
